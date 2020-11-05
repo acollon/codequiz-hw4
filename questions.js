edd23724651e6e13708bac71c7aac6090031fc23
acollon <70421558+acollon@users.noreply.github.com>
2020-11-05 04:20:52 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    title: "This is a common nickname for Detroit:",
+    choices: ["Sin City", "The Motor City", "The Big Apple", "The Windy City"],
+    answer: "The Motor City"
+  },
+  {
+    title: "Detroit is the largest city in this state:",
+    choices: ["Ohio", "Michigan", "Wisconsin", "Indiana"],
+    answer: "Michigan"
+  },
+  {
+    title: "This body of water makes up Detroit's Canadian border:",
+    choices: ["Lake Superior", "Lake Michigan", "Detroit River", "Lake Huron"],
+    answer: "Detroit River"
+  },
+  {
+    title: "It's the largest University located in the City of Detroit:",
+    choices: ["Wayne State University", "University of Dertroit Mercy", "University of Michigan", "Michigan State University"],
+    answer: "Wayne State University"
+  },
+  {
+    title: "The name of the Motown Museum:",
+    choices: ["The Motown Experience", "The home of the Detroit Sound", "Hitsville U.S.A.", "The Motown Museum"],
+    answer: "Hitsville U.S.A."
+  }
+];
